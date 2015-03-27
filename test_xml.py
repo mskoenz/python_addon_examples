74c7770d74caa20d2685bd4ce3af27958b2dd4a3
mskoenz <mskoenz@gmx.net>
2015-03-27 09:44:47 +0100

    15.Commit:	modified xml mskoenz
    hash-word: federal buyer

--- test_xml.py
+++ test_xml.py
@@ -13,9 +13,15 @@ if __name__ == "__main__":
     print("test_xml.py")
     p = xml_parser("./test_data/plot_data.xml")
     
+    ns = namespace()
+    ns.x = 1000000000000000000000000000000000000000000000000000000000000000000000000000000
+    print(ns)
+    
     print(p)
     print(p.plot_option)
-    print(p.plot_option["source"])
+    print(p.plot_option.param())
     print(p.label)
+    print(p.data.info())
     print(p.data.d[0].value())
     print(p.parameter["H"])
+
